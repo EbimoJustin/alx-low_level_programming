b50bbc1bd82da53f7c1e7154af849595f7b1df2f
EbimoJustin <ebisco55@gmail.com>
2023-07-21 03:00:22 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the lowercase alphabet in reverse
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char c
+	for (char c = 'z'; c>= 'a' c--) {
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
